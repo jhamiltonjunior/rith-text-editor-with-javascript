94d9439df65b96adbe1ca81b0ac3e9a9c0de3fad
jhamilton <josehamiltonsantosjunior@gmail.com>
2021-11-06 20:17:23 -0300

    algumas alterações nos arquivos

---
+++ src/js/utils/editorHeader.js
@@ -0,0 +1,15 @@
+const bold = document.createElement('strong')
+const italic = document.createElement('')
+
+// textArea.addEventListener('selectionchange', () => {
+//   text.textContent = textArea.selectionStart
+//   text2.textContent = textArea.selectionEnd
+//   text3.textContent = textArea.selected
+// })
+
+// if (textArea.selectionStart !== undefined) {
+//   const startPosition = textArea.onselectstart
+// }
+// setBold.addEventListener('click', () => {
+//   textArea.innerHTML += '<strong></strong>'
+// })
